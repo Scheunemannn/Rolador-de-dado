4c54a44d876d1dd5dd3a62fa4a089758faf17a0b
Scheunemannn <99615216+Scheunemannn@users.noreply.github.com>
2023-03-10 12:52:44 -0300

    Projeto rolador de Dados

---
+++ Projeto dado/script.js
@@ -0,0 +1,33 @@
+// let faceum = document.getElementById('faceum')
+
+// function cliq(){
+//     return faceum.style.visibility = 'visible'
+// }
+
+
+
+function dados() {
+    let resultado = Math.round(Math.random() * 5) + 1
+
+    if(resultado == 1){
+        faceum.src='imagens/face1.jpg'}
+    else if (resultado == 2){
+        faceum.src='imagens/face2.jpg'
+    }
+    else if (resultado == 3){
+        faceum.src='imagens/face3.jpg'
+    }
+    else if (resultado == 4){
+        faceum.src='imagens/face4.jpg'
+    }
+    else if (resultado == 5){
+        faceum.src='imagens/face5.jpg'
+    }
+    else if (resultado == 6){
+        faceum.src='imagens/face6.jpg'
+    }
+}
+
+
+
+// faceum.innerHTML = <img id="faceum" rc="1 face.png
